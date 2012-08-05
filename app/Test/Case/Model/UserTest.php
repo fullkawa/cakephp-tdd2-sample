5685f40d1311e02c9d46bd345d3151f058989c79
Yoshihiro Furukawa <fullkawa@gmail.com>
2012-08-05 14:06:16 +0900

    Add "UserTest"

---
+++ app/Test/Case/Model/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+App::uses('User', 'Model');
+
+class UserTest extends CakeTestCase {
+
+    public function setup() {
+        parent::setup();
+        $this->User = ClassRegistry::init('User');
+    }
+
+    public function testFindByName() {
+        $user = $this->User->findByName('testuser');
+
+        //debug($user);
+        $this->assertEqual($user['username'], 'testuser');
+    }
+
+    public function testFindByName_nouser() {
+        $user = $this->User->findByName('nouser');
+
+        //debug($user);
+        $this->assertEqual(empty($user), true);
+    }
+}
+?>
